82a72ddcf2f7788c0288432500439f53f4834660
Yi Qiang Ji Zhang <yiqiangjizhang@gmail.com>
2021-03-01 22:11:47 +0100

    Added logistic_map and iterateFunction

--- Exercise-1/iterateFunction.m
+++ Exercise-1/iterateFunction.m
@@ -1,7 +1,11 @@
 function x = iterateFunction(x0, logisticMap, n)
 
+% Define solution vector
 x = zeros(n,1);
+% Set initial condition (each R)
 x(1) = x0;
+
+% Iterate logistic map function
 for i = 2:n
     x(i) = logisticMap(x(i-1));
 end
--- Exercise-1/logistic_map.m
+++ Exercise-1/logistic_map.m
@@ -1,34 +1,61 @@
+%% Exercise 1: Logistic map
+
+%-------------------------------------------------------------------------%
+% Logistic map
+%-------------------------------------------------------------------------%
+
+% Date: 27/02/2021
+% Author/s: Yi Qiang Ji Zhang
+% Subject: Nonlinear Systems, Chaos and Control in Engineering
+% Professor: Antonio Pons & Cristina Masoller
+
+% Clear workspace, command window and close windows
 clear;
 close all;
 clc;
-
+% Set interpreter to latex
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 
-logisticMap = @(x,c) c*x*(1-x);
+%% Logistic map plot
 
+% Declare function
+logisticMap = @(x,r) r*x*(1-x);
+
+% Number of iterations for each r
 numIter = 400;
-cMin = 1.5;
-cMax = 4.5;
-numC = 501;
+% Range of r
+rMin = 0;
+rMax = 4.5;
+% Number of points of R
+numR = 501;
+% Initial condition
 x0 = 0.5;
-k = 100;
 
+% Last 30% constant values (when it is in steady state)
+k = 0.3*numIter;
 
-C = linspace(cMin, cMax, numC);
-m = size(C, 2);
+% Create vector of R's
+R = linspace(rMin, rMax, numR);
+% Length of R
+m = size(R, 2);
+
+% Define points
 points = zeros(numIter-k, m);
 
+% Loop through each R
 for i = 1:m
-    x = iterateFunction(x0, @(x) logisticMap(x,C(i)), numIter);
+    % Calculate logistic map points 
+    x = iterateFunction(x0, @(x) logisticMap(x,R(i)), numIter);
+    % Matrix of points (for each colum R, get the points of logistic map)
     points(:,i) = x(k+1:end);
 end
 
 figure(1);
 hold on;
 title("\textbf{Logistic map}");
-plot(C, points, '.k');
-xlabel("$C$");
+plot(R, points, '.k');
+xlabel("$R$");
 ylabel("Iterations");
 hold off;
