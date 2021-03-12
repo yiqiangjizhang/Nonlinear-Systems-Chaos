02eb2dcc8892a424e0bc41788610a1f81696e587
yiqiangjizhang <yiqiangjizhang@gmail.com>
2021-03-12 21:47:26 +0100

    Added Exercise 9_2

--- 9-Adler_eq/exercise9_3.m
+++ 9-Adler_eq/exercise9_3.m
@@ -21,8 +21,8 @@ set(groot,'defaultLegendInterpreter','latex');
 %% Plot 1
 
 % Constants and Parameters
-w = 1;
-a = 0:0.01:0.99;
+w = 1/sqrt(2);
+a = 0.99;
 
 % Function handle
 f1 = @(t,x,a,w) w-a*sin(x);
@@ -37,32 +37,32 @@ N_steps(j) = length(0:dt:t_final)-1;
 end
 
 % Initial conditions
-x0 = pi/2;
+x0 = [0:0.1:6.5];
 
-for j = 1:length(a)
-    x(j,1) = x0; % x(0) = 1
+% For each h (dt)
+for j=1:length(dt)
     t(j,1) = 0; % We begin at t=0 s
-% Euler method's update loop
-    for i=1:N_steps
-        t(j,i+1) = t(j,i) + dt;
-        x(j,i+1) = x(j,i) + f1(t(j,i),x(j,i),a(j),w)*dt;
-    end
-T(j) = ((t(j,end) -t(j,1))/(x(j,end) - x(j,1)))*(2*pi);
+    for k = 1:length(x0)
 
-figure(1)
-plot(a(j),T(j),'o');
-hold on;
+        x(j,1) = x0(k); % x(0) = 1
+
+        % Euler method's update loop
+        for i=1:N_steps(j)
+            t(j,i+1) = t(j,i) + dt(j);
+            x(j,i+1) = x(j,i) + f1(t(j,i),x(j,i),a,w)*dt(j);
+        end
+        plot(t(j,:),x(j,:));
+        hold on;
+
+    end
 end
 
-
-xlabel('a')
-ylabel('$T$')
+xlim([0 15])
+ylim([0 8])
+xlabel('Time')
+ylabel('$\theta$')
 title('Plot')
+legend()
 box on
 grid minor
-
-% Analytical
-T_analytical = 2*pi./sqrt(w^2 - a.^2);
-plot(a,T_analytical)
-
-
+hold off;
