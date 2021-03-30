ee5be7e1a12c553395933cbfda06a44a295dcdd9
yiqiangjizhang <yiqiangjizhang@gmail.com>
2021-03-30 15:52:56 +0200

    Exercise 10 - Delayed Logistic equation

--- 10-Delayed-Logistic-Equation/exercise10.m
+++ 10-Delayed-Logistic-Equation/exercise10.m
@@ -1,10 +1,10 @@
-%% Exercise 9
+%% Exercise 10
 
 %-------------------------------------------------------------------------%
-% Adler Equation
+% Delayed logistic equation
 %-------------------------------------------------------------------------%
 
-% Date: 09/03/2021
+% Date: 12/03/2021
 % Author/s: Yi Qiang Ji Zhang
 % Subject: Nonlinear Systems, Chaos and Control in Engineering
 % Professor: Antonio Pons & Cristina Masoller
@@ -18,36 +18,46 @@ set(groot,'defaultAxesTickLabelInterpreter','latex');
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 
-%% Plot 1
+%% Delayed Adler equation
 
+% Paramter tau
+tau = [1];
 
-for tau=0:0.5:1
-    solve_delay(tau);
+% For different tau values
+for i=1:1:length(tau)
+    solve_delay(tau(i));
 end
 
 
-
-
-xlabel('Time')
-ylabel('$\theta$')
-title('Plot')
-legend()
+% Plot
+plot_pdf = figure(1);
+xlabel('Time units')
+ylabel('$y$')
+title('\textbf{Delayed logistic equation for $\tau = 1$}')
+% legend()
 box on
 grid minor
-hold off;
 
-function solve_delay1(tau)
-tau = 1;
-ic = [0.5];
-tspan = [0 100];
-lambda = 1.8;
-sol = dde23(@f,tau,ic,tspan);
-plot(sol.x,sol.y(1,:),'r-')
-hold on;
-function v(t,y,Z);
+% Save pdf
+set(plot_pdf, 'Units', 'Centimeters');
+pos = get(plot_pdf, 'Position');
+set(plot_pdf, 'PaperPositionMode', 'Auto', 'PaperUnits', 'Centimeters', ...
+    'PaperSize',[pos(3), pos(4)]);
+print(plot_pdf, 'delayed_logistic_eq.pdf', '-dpdf', '-r0');
+
+% Save png
+print(gcf,'delayed_logistic_eq.png','-dpng','-r600');
+
+
+% Function definition
+function solve_delay(tau)
+    ic = [0.5];
+    tspan = [0 100];
+    lambda = 1.8;
+    sol = dde23(@f,tau,ic,tspan);
+    plot(sol.x,sol.y(1,:),'r-')
+    hold on;
+    function v=f(t,y,Z)
         v = [lambda*y(1).*(1-Z(1))];
+    end
 end
-end
-
-
-
--- 10-Delayed-Logistic-Equation/v.m
+++
@@ -1,3 +0,0 @@
-function v(t,y,Z);
-        v = [lambda*y(1).*(1-Z(1))];
-end
